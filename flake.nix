2d45149c9261879b5507288f05518436c24aa6cc
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-05-28 18:51:01 +0200

    flake: add nixpkgs 20.03 and unstable

--- flake.nix
+++ flake.nix
@@ -5,6 +5,24 @@
   edition = 201909;
 
   inputs = {
+    nixpkgs-unstable = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+
+      # branch is master
+      rev = "2738ca86bd623934d816bef90f1867002c119950";
+    };
+
+    nixpkgs-20-03 = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+
+      # branch is release-20.03
+      rev = "e985ffea2d640bb6fe7d5ef7aa968b2b7d107f47";
+    };
+
     home-manager = {
       type = "github";
       owner = "rycee";
@@ -15,7 +33,7 @@
     };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs-unstable, nixpkgs-20-03, home-manager }: {
 
   };
 }
