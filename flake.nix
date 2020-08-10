3f2588dd6cd50d4bd25a0f4a53e4d967c35b043e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-08-10 14:45:19 -0700

    another wip

--- flake.nix
+++ flake.nix
@@ -2,28 +2,12 @@
   description = "Soxin ...";
 
   inputs = {
-    nixpkgs-master.url = "nixpkgs/master";
-    nixpkgs-release.url = "nixpkgs/release-20.03";
+    nixpkgs.url = "nixpkgs/master";
     # home-manager.url = "github:rycee/home-manager/bqv-flakes";
     home-manager.url = "git+file:///yl/code/stories/opensource/home-manager/flakes/github.com/rycee/home-manager";
   };
 
-  outputs = inputs@{ self, nixpkgs-master, nixpkgs-release, home-manager }: {
-    nixosConfigurations = {
-      niximg = nixpkgs-release.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ({ modulesPath, ... }: {
-        imports = [
-          "${modulesPath}/installer/cd-dvd/iso-image.nix"
-          ];
-
-          isoImage.makeEfiBootable = true;
-          isoImage.makeUsbBootable = true;
-          networking.networkmanager.enable = true;
-        })
-        ];
-      };
-    };
+  outputs = inputs@{ self, nixpkgs, home-manager }: {
+    nixosModules = import ./modules/list.nix;
   };
 }
