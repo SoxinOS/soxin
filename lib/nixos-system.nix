2033afafb5eaf570724f7d88d093805d0799f4f6
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-09-12 22:46:24 +0200

    lib: nixosSystem: add home-manager NixOS wrapper
    By adding this wrapper, we add extra functionality to the
    home-manager.users option in the NixOS configuration part. In this case
    we make the global NixOS configuration available to the home-manager one
    under the argument `super`.
    
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- lib/nixos-system.nix
+++ lib/nixos-system.nix
@@ -4,5 +4,21 @@
 lib.nixosSystem (args // {
   modules = lib.concat modules [
     home-manager.nixosModules.home-manager
+    {
+      # This is required when using flakes.
+      home-manager.useGlobalPkgs = true;
+    }
+
+    # Allow accessing the parent NixOS configuration.
+    ({ config, ... }: {
+      options.home-manager.users = lib.mkOption {
+        type = lib.types.attrsOf (lib.types.submoduleWith {
+          modules = [ ];
+          specialArgs = {
+            super = config;
+          };
+        });
+      };
+    })
   ];
 })
