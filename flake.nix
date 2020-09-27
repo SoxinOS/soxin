627274af28353b8fbad739ceef8af1879a32eca6
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-09-27 16:04:46 +0200

    flake: make home-manager follow nixpkgs
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager } @ inputs:
