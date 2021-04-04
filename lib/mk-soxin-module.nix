55685813b63cdb42eed90ff643b8e65330130d59
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2021-04-04 22:12:12 +0200

    lib: add mkSoxinModule (#11)
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

---
+++ lib/mk-soxin-module.nix
@@ -0,0 +1,37 @@
+{ lib, modules }:
+
+let
+  inherit (lib)
+    optionalAttrs
+    mkEnableOption
+    mkOption
+    recursiveUpdate
+    types
+  ;
+in
+{ config
+, name
+, includeKeyboardLayout ? false
+, includeTheme ? false
+, extraOptions ? { }
+}:
+
+recursiveUpdate
+{
+  enable = mkEnableOption name;
+
+  keyboardLayout = optionalAttrs includeKeyboardLayout (mkOption {
+    type = modules.keyboard.layoutModule;
+    default = config.soxin.settings.keyboard.defaultLayout;
+    description = "Keyboard layout to use for ${name}.";
+  });
+
+  theme = optionalAttrs includeTheme (mkOption {
+    type = with types; oneOf [ str modules.themes.themeModule ];
+    default = config.soxin.settings.theme;
+    description = "Theme to use for ${name}.";
+    apply = value: if builtins.isString value then config.soxin.themes.${value}.${name}
+                   else value.${name};
+  });
+}
+extraOptions
