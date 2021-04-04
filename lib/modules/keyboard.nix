456be431659ff320b1c9b793658adf4021bd210e
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2021-04-04 15:16:35 +0200

    mkSoxinModule: improvements (#25)
    * better handling of types
    * switch to using lib instead of utils

---
+++ lib/modules/keyboard.nix
@@ -0,0 +1,46 @@
+{ lib }:
+
+let
+  inherit (lib)
+    mkDefault
+    mkOption
+    types
+  ;
+in
+{
+  layoutModule = types.submodule ({ config, ... }: {
+    options = {
+      x11 = {
+        layout = mkOption {
+          type = types.str;
+          default = "us";
+          example = "fr";
+          description = "Keyboard layout";
+        };
+
+        variant = mkOption {
+          type = types.str;
+          default = "";
+          example = "colemak";
+          description = "Keyboard variant";
+        };
+      };
+
+      console = {
+        keyMap = mkOption {
+          type = types.str;
+          example = "us";
+          description = ''
+            The keyboard mapping table for the virtual consoles. The default is
+            the layout associated with the keyMap but you may need to change this
+            depending on your keyboard variant.
+          '';
+        };
+      };
+    };
+
+    config = {
+      console.keyMap = mkDefault config.x11.layout;
+    };
+  });
+}
