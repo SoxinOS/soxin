01043185c53cbf64b106d7f35b0b04f68ba66d5c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-15 09:04:44 -0700

    add missing i3 support file

--- modules/programs/rofi/default.nix
+++ modules/programs/rofi/default.nix
@@ -57,9 +57,10 @@ in
   config = mkIf cfg.enable (mkMerge [
     {
       soxin.programs.rofi.modi = mkIf (cfg.i3.enable) {
-        i3Workspaces = "${pkgs.rofi-i3-support}/bin/i3-switch-workspaces";
-        i3RenameWorkspace = "${pkgs.rofi-i3-support}/bin/i3-rename-workspace";
         i3MoveContainer = "${pkgs.rofi-i3-support}/bin/i3-move-container";
+        i3RenameWorkspace = "${pkgs.rofi-i3-support}/bin/i3-rename-workspace";
+        i3SwapWorkspaces = "${pkgs.rofi-i3-support}/bin/i3-swap-workspaces";
+        i3Workspaces = "${pkgs.rofi-i3-support}/bin/i3-switch-workspaces";
       };
     }
 
