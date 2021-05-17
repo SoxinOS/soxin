97cd05365be7cd2d8e051315b95ce5400402b75d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-17 14:28:11 -0700

    fix the tmux gruvbox plugin

--- modules/themes/gruvbox.nix
+++ modules/themes/gruvbox.nix
@@ -133,66 +133,65 @@ with lib;
       tmux = {
         plugins =
           let
-            tmux-gruvbox-dark-plugin-src = pkgs.writeText "tmux-gruvbox-dark.tmux" ''
-              # pane number display
-              set-option -g display-panes-active-colour colour250 #fg2
-              set-option -g display-panes-colour colour237 #bg1
+            tmux-gruvbox-dark-plugin-src = with pkgs;
+              runCommandNoCCLocal "tmux-gruvbox-dark"
+                {
+                  theme = writeText "tmux-gruvbox-dark.tmux" ''
+                    # pane number display
+                    set-option -g display-panes-active-colour colour250 #fg2
+                    set-option -g display-panes-colour colour237 #bg1
 
-              # clock
-              set-window-option -g clock-mode-colour colour109 #blue
+                    # clock
+                    set-window-option -g clock-mode-colour colour109 #blue
 
-              # bell
-              set-window-option -g window-status-bell-style fg=colour235,bg=colour167 #bg, red
+                    # bell
+                    set-window-option -g window-status-bell-style fg=colour235,bg=colour167 #bg, red
 
-              ## Theme settings mixed with colors (unfortunately, but there is no cleaner way)
-              set-option -g status-justify "left"
-              set-option -g status-left-length "80"
-              set-option -g status-right-length "80"
-              set-window-option -g window-status-separator ""
+                    ## Theme settings mixed with colors (unfortunately, but there is no cleaner way)
+                    set-option -g status-justify "left"
+                    set-option -g status-left-length "80"
+                    set-option -g status-right-length "80"
+                    set-window-option -g window-status-separator ""
 
-              set-option -g status-left "#[fg=colour248, bg=colour241] #S #[fg=colour241, bg=colour237, nobold, noitalics, nounderscore]"
-              set-option -g status-right "#[fg=colour239, bg=colour237, nobold, nounderscore, noitalics]#{prefix_highlight}#[fg=colour246,bg=colour239] %Y-%m-%d  %H:%M #[fg=colour248, bg=colour239, nobold, noitalics, nounderscore]#[fg=colour237, bg=colour248] #h "
+                    set-option -g status-left "#[fg=colour248, bg=colour241] #S #[fg=colour241, bg=colour237, nobold, noitalics, nounderscore]"
+                    set-option -g status-right "#[fg=colour239, bg=colour237, nobold, nounderscore, noitalics]#{prefix_highlight}#[fg=colour246,bg=colour239] %Y-%m-%d  %H:%M #[fg=colour248, bg=colour239, nobold, noitalics, nounderscore]#[fg=colour237, bg=colour248] #h "
 
-              set-window-option -g window-status-current-format "#[fg=colour239, bg=colour248, :nobold, noitalics, nounderscore]#[fg=colour239, bg=colour214] #I #[fg=colour239, bg=colour214, bold] #W #[fg=colour214, bg=colour237, nobold, noitalics, nounderscore]"
-              set-window-option -g window-status-format "#[fg=colour237,bg=colour239,noitalics]#[fg=colour223,bg=colour239] #I #[fg=colour223, bg=colour239] #W #[fg=colour239, bg=colour237, noitalics]"
+                    set-window-option -g window-status-current-format "#[fg=colour239, bg=colour248, :nobold, noitalics, nounderscore]#[fg=colour239, bg=colour214] #I #[fg=colour239, bg=colour214, bold] #W #[fg=colour214, bg=colour237, nobold, noitalics, nounderscore]"
+                    set-window-option -g window-status-format "#[fg=colour237,bg=colour239,noitalics]#[fg=colour223,bg=colour239] #I #[fg=colour223, bg=colour239] #W #[fg=colour239, bg=colour237, noitalics]"
 
-              # default statusbar colors
-              set-option -g status-style bg=colour237,fg=colour223,none
-              set-option -g status-left-style none
-              set-option -g status-right-style none
+                    # default statusbar colors
+                    set-option -g status-style bg=colour237,fg=colour223,none
+                    set-option -g status-left-style none
+                    set-option -g status-right-style none
 
-              # default window title colors
-              set-window-option -g window-status-style bg=colour214,fg=colour237,none
+                    # default window title colors
+                    set-window-option -g window-status-style bg=colour214,fg=colour237,none
 
-              set-window-option -g window-status-activity-style bg=colour237,fg=colour248,none
+                    set-window-option -g window-status-activity-style bg=colour237,fg=colour248,none
 
-              # active window title colors
-              set-window-option -g window-status-current-style bg=default,fg=colour237
+                    # active window title colors
+                    set-window-option -g window-status-current-style bg=default,fg=colour237
 
-              # pane border
-              set-option -g pane-active-border-style bg=colour250,fg=colour237
-              set-option -g pane-border-style bg=colour237,fg=colour250
+                    # pane border
+                    set-option -g pane-active-border-style bg=colour250,fg=colour237
+                    set-option -g pane-border-style bg=colour237,fg=colour250
 
-              # message infos
-              set-option -g message-style bg=colour239,fg=colour223
+                    # message infos
+                    set-option -g message-style bg=colour239,fg=colour223
 
-              # writting commands inactive
-              set-option -g message-command-style bg=colour239,fg=colour223
-            '';
+                    # writting commands inactive
+                    set-option -g message-command-style bg=colour239,fg=colour223
+                  '';
+                } ''
+                mkdir -p $out
+                ln -s $theme $out/gruvbox_dark.tmux
+              '';
           in
-          singleton (pkgs.tmuxPlugins.mkTmuxPlugin rec {
+          singleton (pkgs.tmuxPlugins.mkTmuxPlugin {
             pluginName = "gruvbox-dark";
             inherit (pkgs.vimPlugins.gruvbox-community) version;
             src = tmux-gruvbox-dark-plugin-src;
-            unpackPhase = ":";
-            installPhase = ''
-              ls -la
-              env
-              exit 1
-
-              mkdip -p $out/share/tmux-plugins
-              cp $sourceRoot $out/share/tmux-plugins/gruvbox-dark.tmux
-            '';
+            # unpackPhase = ":";
           });
       };
 
