3f41c3cedac602c4ee45f10ab74a78c1c6728266
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-17 11:42:21 -0700

    simplify home-manager flake integration

--- lib/nixos-system.nix
+++ lib/nixos-system.nix
@@ -1,7 +1,9 @@
 { self, lib, home-manager }:
 
+with lib;
+
 { modules ? [ ], globalSpecialArgs ? { }, nixosSpecialArgs ? { }, hmSpecialArgs ? { }, ... } @ args:
-lib.nixosSystem (lib.recursiveUpdate (removeAttrs args [ "globalSpecialArgs" "nixosSpecialArgs" "hmSpecialArgs" ]) {
+nixosSystem (recursiveUpdate (removeAttrs args [ "globalSpecialArgs" "nixosSpecialArgs" "hmSpecialArgs" ]) {
   specialArgs = {
     mode = "NixOS";
     soxin = self;
@@ -9,36 +11,26 @@ lib.nixosSystem (lib.recursiveUpdate (removeAttrs args [ "globalSpecialArgs" "ni
 
   modules =
     modules
+    # include all Soxin modules
     ++ (builtins.attrValues self.nixosModules)
+    # include all home-manager modules
     ++ (builtins.attrValues home-manager.nixosModules)
-    ++ [
-      {
-        _module.args = { inherit home-manager; };
+    # configure the arguments
+    ++ (singleton {
+      _module.args = { inherit home-manager; };
+    })
+    # configure home-manager
+    ++ (singleton {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+
+      home-manager.extraSpecialArgs = {
+        mode = "home-manager";
+        soxin = self;
       }
+      // globalSpecialArgs
+      // hmSpecialArgs;
 
-      # Required when using flakes.
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-      }
-
-      # Override home-manager per-user submodule to add our own modules to it,
-      # and to pass the argument `mode`.
-      ({ config, ... }: {
-        options.home-manager.users = lib.mkOption {
-          type = lib.types.attrsOf (lib.types.submoduleWith {
-            modules =
-              (builtins.attrValues self.nixosModules)
-              ++ [{ _module.args = { inherit home-manager; }; }];
-
-            specialArgs = {
-              mode = "home-manager";
-              soxin = self;
-            }
-            // globalSpecialArgs
-            // hmSpecialArgs;
-          });
-        };
-      })
-    ];
+      home-manager.sharedModules = builtins.attrValues self.nixosModules;
+    });
 })
