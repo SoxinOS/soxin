917ad5c737505aa9c37a147b5fabf150ad3aa1ec
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-17 11:57:11 -0700

    fix the nixos modules

--- lib/nixos-system.nix
+++ lib/nixos-system.nix
@@ -2,37 +2,78 @@
 
 with lib;
 
-{ modules ? [ ], globalSpecialArgs ? { }, nixosSpecialArgs ? { }, hmSpecialArgs ? { }, ... } @ args:
+{
+  # The global modules are included in both NixOS and home-manager.
+  globalModules ? [ ]
+
+  # Home-manager specific modules.
+, hmModules ? [ ]
+
+  # NixOS specific modules.
+, nixosModules ? [ ]
+
+  # The global extra arguments are included in both NixOS and home-manager.
+, globalSpecialArgs ? { }
+
+  # Home-manager specific extra arguments.
+, hmSpecialArgs ? { }
+
+  # NixOS specific extra arguments.
+, nixosSpecialArgs ? { }
+
+, ...
+} @ args:
 nixosSystem (recursiveUpdate (removeAttrs args [ "globalSpecialArgs" "nixosSpecialArgs" "hmSpecialArgs" ]) {
   specialArgs = {
+    # the mode allows us to tell at what level we are within the modules.
     mode = "NixOS";
+
+    # send soxin to all NixOS modules
     soxin = self;
-  } // globalSpecialArgs // nixosSpecialArgs;
+  }
+  # include the global special arguments.
+  // globalSpecialArgs
+  # include the NixOS special arguments.
+  // nixosSpecialArgs;
 
   modules =
-    modules
+    # include the global modules
+    globalModules
+    # include the NixOS modules
+    ++ nixosModules
     # include all Soxin modules
     ++ (builtins.attrValues self.nixosModules)
     # include all home-manager modules
     ++ (builtins.attrValues home-manager.nixosModules)
-    # configure the arguments
-    ++ (singleton {
-      _module.args = { inherit home-manager; };
-    })
+
+    # send home-manager down to NixOS modules
+    ++ (singleton { _module.args = { inherit home-manager; }; })
+
     # configure home-manager
     ++ (singleton {
+      # tell home-manager to use the global (as in NixOS system-level) pkgs and
+      # install all  user packages through the users.users.<name>.packages.
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
 
+      #
       home-manager.extraSpecialArgs = {
+        # the mode allows us to tell at what level we are within the modules.
         mode = "home-manager";
+        # send soxin to all NixOS modules
         soxin = self;
       }
+      # include the global special arguments.
       // globalSpecialArgs
+      # include the NixOS special arguments.
       // hmSpecialArgs;
 
-      home-manager.sharedModules = singleton {
-        imports = builtins.attrValues self.nixosModules;
-      };
+      home-manager.sharedModules =
+        # include the global modules
+        globalModules
+        # include the home-manager modules
+        ++ hmModules
+        # include all Soxin modules
+        ++ (builtins.attrValues self.nixosModules);
     });
 })
