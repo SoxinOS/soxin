81f7cc4f8f3a3d418e6e367dc9476eae40964d8a
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-17 16:41:23 -0700

    first part

--- lib/modules/themes.nix
+++ lib/modules/themes.nix
@@ -1,14 +1,19 @@
-{ lib }:
-
+{ lib, neovim }:
 let
   inherit (lib)
     mkOption
     types
-  ;
+    ;
+
 in
 rec {
-  themeModule = types.submodule {
+  themeModule = with types; submodule {
     options = {
+      neovim = mkOption {
+        type = neovimModule;
+        default = { };
+      };
+
       rofi = mkOption {
         type = rofiModule;
         default = { };
@@ -16,10 +21,35 @@ rec {
     };
   };
 
-  rofiModule = types.submodule {
+
+  neovimModule = with types; submodule {
+    options = {
+      plugins = mkOption {
+        type = listOf (either package neovim.pluginWithConfigModule);
+        default = [ ];
+        example = literalExample ''
+          with pkgs.vimPlugins; [
+            yankring
+            vim-nix
+            { plugin = vim-startify;
+              config = "let g:startify_change_to_vcs_root = 0";
+            }
+          ]
+        '';
+        description = ''
+          List of vim plugins to install optionally associated with
+          configuration to be placed in init.vim.
+          </para><para>
+          This option is mutually exclusive with <varname>configure</varname>.
+        '';
+      };
+    };
+  };
+
+  rofiModule = with types; submodule {
     options = {
       name = mkOption {
-        type = with types; nullOr str;
+        type = nullOr str;
         default = null;
       };
     };
