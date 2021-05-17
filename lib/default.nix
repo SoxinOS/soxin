610eb1d3bc264255944b953ed859e08f781e718c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-17 12:21:44 -0700

    sorting only

--- lib/default.nix
+++ lib/default.nix
@@ -1,13 +1,9 @@
 { lib, self, home-manager, ... }:
 
 rec {
+  homeManagerConfiguration = import ./home-manager-configuration.nix { inherit self lib home-manager; };
   mkSoxinModule = import ./mk-soxin-module.nix { inherit lib modules; };
   modules = import ./modules { inherit lib; };
-  nixosSystem = import ./nixos-system.nix {
-    inherit self lib home-manager;
-  };
-  homeManagerConfiguration = import ./home-manager-configuration.nix {
-    inherit self lib home-manager;
-  };
+  nixosSystem = import ./nixos-system.nix { inherit self lib home-manager; };
   overlaysToPkgs = import ./overlays-to-pkgs.nix { inherit lib; };
 }
