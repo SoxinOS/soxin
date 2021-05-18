ce889b14719b6b27e570f515a3295db9f1542b75
Wael Nasreddine <wael.nasreddine@gmail.com>
2021-05-18 16:20:33 -0700

    Update lib/modules/default.nix
    Co-authored-by: risson <18313093+rissson@users.noreply.github.com>

--- lib/modules/default.nix
+++ lib/modules/default.nix
@@ -3,5 +3,5 @@
 rec {
   keyboard = import ./keyboard.nix { inherit lib; };
   neovim = import ./neovim.nix { inherit lib; };
-  themes = import ./themes.nix { inherit lib neovim zsh; };
+  themes = import ./themes.nix { inherit lib neovim; };
 }
