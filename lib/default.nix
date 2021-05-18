9313c3743a8329cb071c4b0afec781cb1e72422c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-17 17:15:27 -0700

    this one too

--- lib/default.nix
+++ lib/default.nix
@@ -3,8 +3,6 @@
 rec {
   mkSoxinModule = import ./mk-soxin-module.nix { inherit lib modules; };
   modules = import ./modules { inherit lib; };
-  nixosSystem = import ./nixos-system.nix {
-    inherit self lib home-manager;
-  };
+  nixosSystem = import ./nixos-system.nix { inherit self lib home-manager; };
   overlaysToPkgs = import ./overlays-to-pkgs.nix { inherit lib; };
 }
