009d2c687c32e513593a6453f6d1e4e940409ceb
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-17 17:35:01 -0700

    1

--- lib/modules/default.nix
+++ lib/modules/default.nix
@@ -4,4 +4,5 @@ rec {
   keyboard = import ./keyboard.nix { inherit lib; };
   neovim = import ./neovim.nix { inherit lib; };
   themes = import ./themes.nix { inherit lib neovim zsh; };
+  zsh = import ./zsh.nix { inherit lib; };
 }
