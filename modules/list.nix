d457e88b280015184efbeb1386a44367399fbc83
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-23 17:54:44 -0700

    modules: sort the list of modules per type (#40)

--- modules/list.nix
+++ modules/list.nix
@@ -1,15 +1,19 @@
 {
-  keyboard = ./settings/keyboard.nix;
-  theme = ./settings/theme.nix;
-
-  themes = ./themes;
-
+  # hardware
   bluetooth = ./hardware/bluetooth.nix;
 
+  # programs
   neovim = ./programs/neovim;
   rbrowser = ./programs/rbrowser;
   rofi = ./programs/rofi;
   termite = ./programs/termite;
   tmux = ./programs/tmux;
   zsh = ./programs/zsh;
+
+  # settings
+  keyboard = ./settings/keyboard.nix;
+  theme = ./settings/theme.nix;
+
+  # themes
+  themes = ./themes;
 }
