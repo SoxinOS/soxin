07d6d23c75d0cb2a0121cd5b88c802c30360b34c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-23 20:22:31 -0700

    programs.less: init module (#40)

--- modules/list.nix
+++ modules/list.nix
@@ -6,6 +6,7 @@
   autorandr = ./programs/autorandr;
   git = ./programs/git;
   keybase = ./programs/keybase;
+  less = ./programs/less;
   neovim = ./programs/neovim;
   rbrowser = ./programs/rbrowser;
   rofi = ./programs/rofi;
