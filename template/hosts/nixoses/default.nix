90f05d6003f565c84e6b56f4ec477db67ccb2f1d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-05-30 20:46:03 -0700

    fix the nixoses

--- template/hosts/nixoses/default.nix
+++ template/hosts/nixoses/default.nix
@@ -1,23 +1,27 @@
 inputs@{ self, deploy-rs, nixpkgs, ... }:
 
 {
-  example = rec {
-    # System architecture.
-    system = "x86_64-linux";
-    # <name> of the channel to be used. Defaults to `nixpkgs`
-    channelName = "nixpkgs";
-    # Extra arguments to be passed to the modules.
-    extraArgs = { };
-    # Host specific configuration.
-    modules = [ ./example/configuration.nix ];
+  minimal =
+    let
+      system = "x86_64-linux";
+    in
+    {
+      # System architecture.
+      inherit system;
+      # <name> of the channel to be used. Defaults to `nixpkgs`
+      channelName = "nixpkgs";
+      # Extra arguments to be passed to the modules.
+      extraArgs = { };
+      # Host specific configuration.
+      modules = [ ./minimal/configuration.nix ];
 
-    deploy = {
-      hostname = "host.example.com";
-      profiles.system = {
-        sshUser = "root";
-        user = "root";
-        path = deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.example;
+      deploy = {
+        hostname = "host.minimal.com";
+        profiles.system = {
+          sshUser = "root";
+          user = "root";
+          path = deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.minimal;
+        };
       };
     };
-  };
 }
