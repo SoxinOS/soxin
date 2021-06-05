f320f7e32b73a39352f8f072a5033f1baea396cc
Shahrukh Khan <shahrukh330@gmail.com>
2021-06-04 22:41:36 -0500

    Update modules/settings/programmingLanguages.nix
    Co-authored-by: risson <18313093+rissson@users.noreply.github.com>

--- modules/settings/programmingLanguages.nix
+++ modules/settings/programmingLanguages.nix
@@ -4,7 +4,7 @@ with lib;
 {
   options = {
     soxin.settings.progammingLanguages = mkOption {
-      type = with types; nullOr (enum (mapAttrsToList (n: _: n) config.soxin.programmingLanguages));
+      type = with types; listOf (enum (mapAttrsToList (n: _: n) config.soxin.programmingLanguages));
       apply = value: config.soxin.programmingLanguages.${value};
     };
   };
