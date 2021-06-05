2de01935091aa2f1d33c5c5a7a79715e0bbdffdb
Shahrukh Khan <shahrukh330@gmail.com>
2021-06-04 22:41:16 -0500

    Update modules/settings/tools.nix
    Co-authored-by: risson <18313093+rissson@users.noreply.github.com>

--- modules/settings/tools.nix
+++ modules/settings/tools.nix
@@ -4,7 +4,7 @@ with lib;
 {
   options = {
     soxin.settings.tools = mkOption {
-      type = with types; nullOr (enum (mapAttrsToList (n: _: n) config.soxin.tools));
+      type = with types; listOf (enum (mapAttrsToList (n: _: n) config.soxin.tools));
       apply = value: config.soxin.tools.${value};
     };
   };
