2c37a6f7ad6119a9d9453514689464a5e8e4f64f
Shahrukh Khan <shahrukh330@gmail.com>
2021-06-25 21:30:10 -0500

    Test

--- modules/settings/programmingLanguage.nix
+++ modules/settings/programmingLanguage.nix
@@ -5,7 +5,7 @@ with lib;
   options = {
     soxin.settings.programmingLanguage = mkOption {
       type = with types; listOf (enum (mapAttrsToList (n: _: n) config.soxin.programmingLanguages));
-      apply = value: config.soxin.programmingLanguages.${value};
+      apply = value: config.soxin.programmingLanguages.${value}.enable = true;
     };
   };
 }
