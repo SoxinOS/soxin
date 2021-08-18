6758b1e17f9ef635f6c43876a76444dc9fc5615e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-08-18 14:25:14 -0700

    Revert "enable verbose mode"
    This reverts commit 21897e8b59c618a1f3cf3d4954e87fd6329f5b3d.

--- lib/system-flake.nix
+++ lib/system-flake.nix
@@ -164,7 +164,6 @@ let
                     # install all  user packages through the users.users.<name>.packages.
                     home-manager.useGlobalPkgs = true;
                     home-manager.useUserPackages = true;
-                    home-manager.verbose = true;
 
                     home-manager.extraSpecialArgs = {
                       inherit soxin soxincfg home-manager;
