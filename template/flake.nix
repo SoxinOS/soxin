f4ce66d9908d30693ad75848057df73e05b5ab9c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-12-27 17:24:45 -0800

    update the flake-utils-plus libarary

--- template/flake.nix
+++ template/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     deploy-rs.url = "github:serokell/deploy-rs";
-    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.1.0";
+    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.1";
     home-manager.url = "github:nix-community/home-manager/release-21.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
