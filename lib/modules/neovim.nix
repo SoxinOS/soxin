a8fe36b1b7a5ba60773ce0718614b2c363a5020c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-05-11 21:55:36 -0700

    soxin: inherit the option as-is from home-manager

--- lib/modules/neovim.nix
+++ lib/modules/neovim.nix
@@ -1,4 +1,4 @@
-{ nixpkgs, ... }:
+{ nixpkgs, home-manager, ... }:
 
 let
   inherit (nixpkgs) lib;
@@ -8,25 +8,13 @@ let
     types
     ;
 
+  hmModuleEval = home-manager.homeManagerConfiguration {
+    configuration = { };
+    username = "foo";
+    pkgs = nixpkgs;
+    check = false;
+  };
 in
 {
-  # TODO: Bring this from home-manager!
-  pluginWithConfigModule = types.submodule {
-    options = {
-      config = mkOption {
-        type = types.lines;
-        description = "vimscript for this plugin to be placed in init.vim";
-        default = "";
-      };
-
-      optional = mkEnableOption "optional" // {
-        description = "Don't load by default (load with :packadd)";
-      };
-
-      plugin = mkOption {
-        type = types.package;
-        description = "vim plugin";
-      };
-    };
-  };
+  pluginWithConfigModule = hmModuleEval.options.programs.neovim.plugins.type;
 }
