cc1cee2fbfb82a4303327074990e0d463e01f0f5
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-05-23 09:59:06 -0700

    comment and indent

--- lib/modules/neovim.nix
+++ lib/modules/neovim.nix
@@ -8,14 +8,12 @@ let
     types
     ;
 
-  hmModuleEval = home-manager.lib.homeManagerConfiguration rec {
-    system="x86_64-linux";
+  hmModuleEval = home-manager.lib.homeManagerConfiguration {
+    system = "x86_64-linux"; # system don't matter here, we just need the type of the plugins option.
     configuration = { };
     username = "nouser";
-    homeDirectory="/home/nouser";
+    homeDirectory = "/home/nouser";
     pkgs = builtins.getAttr system nixpkgs.outputs.legacyPackages;
   };
 in
-{
-  pluginWithConfigModule = hmModuleEval.options.programs.neovim.plugins.type;
-}
+{ pluginWithConfigModule = hmModuleEval.options.programs.neovim.plugins.type; }
