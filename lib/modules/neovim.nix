6d48240a9551c9a6549cbe179b240197a1c62e45
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2022-05-23 10:32:29 -0700

    fix the build

--- lib/modules/neovim.nix
+++ lib/modules/neovim.nix
@@ -8,12 +8,19 @@ let
     types
     ;
 
-  hmModuleEval = home-manager.lib.homeManagerConfiguration {
-    system = "x86_64-linux"; # system don't matter here, we just need the type of the plugins option.
-    configuration = { };
-    username = "nouser";
-    homeDirectory = "/home/nouser";
-    pkgs = builtins.getAttr system nixpkgs.outputs.legacyPackages;
-  };
+  hmModuleEval =
+    let
+      # system don't matter here, we just need the type of the plugins option.
+      system = "x86_64-linux";
+
+      # username also don't matter here.
+      username = "nouser";
+    in
+    home-manager.lib.homeManagerConfiguration {
+      inherit system username;
+      configuration = { };
+      homeDirectory = "/home/${username}";
+      pkgs = builtins.getAttr system nixpkgs.outputs.legacyPackages;
+    };
 in
 { pluginWithConfigModule = hmModuleEval.options.programs.neovim.plugins.type; }
