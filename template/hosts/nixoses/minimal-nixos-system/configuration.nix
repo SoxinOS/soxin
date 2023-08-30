e7f94bd0ff28dba1e783c1c65966f4cfcdba2ae6
Wael Nasreddine <wael.nasreddine@gmail.com>
2023-08-29 23:26:39 -0700

    enable zsh and set system state version

--- template/hosts/nixoses/minimal-nixos-system/configuration.nix
+++ template/hosts/nixoses/minimal-nixos-system/configuration.nix
@@ -12,9 +12,13 @@ in
     soxincfg.nixosModules.profiles.workstation.nixos.local
   ];
 
+  programs.zsh.enable = true;
+
   # the user nick is created by the core profiles which is automatically added
   # to the configuration of all supported systems.
   home-manager.users.nick = { ... }: {
     imports = singleton ./home.nix;
   };
+
+  system.stateVersion = "23.05";
 }
