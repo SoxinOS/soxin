5481b9b8e0ab0684f05928aad5c5837653e77dec
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-11-09 19:03:45 -0800

    send the inputs instead of expecting each host to come with it

--- lib/mk-flake.nix
+++ lib/mk-flake.nix
@@ -382,7 +382,7 @@ flake-utils-plus.lib.mkFlake (recursiveUpdate soxinSystemFlake otherArguments)
   // {
   homeConfigurations = (mapAttrs
     (_: host: soxin.lib.homeManagerConfiguration (host // {
-      inherit hmSpecialArgs;
+      inherit hmSpecialArgs inputs;
 
       modules =
         host.modules
